a945535dacfd8407a89dd8b48d1ca5c8154ecf45
vasim kureshi <kkvasim417@gamail.com>
2023-08-19 15:14:09 -0700

    v1

---
+++ src/com/dl/basic/Eg3.java
@@ -0,0 +1,15 @@
+package com.dl.basic;
+
+public class Eg3 {
+
+	public static void main(String[] args) {
+ String ch ="v";
+ System.out.println(ch);
+ double f=1.1;
+ System.out.println(f);
+		
+			
+
+	}
+
+}
