c1f07dbd0faf7b77fd7b860c8a932052e318f4ab
vasim kureshi <kkvasim417@gamail.com>
2023-08-20 17:15:59 -0700

    v1

---
+++ src/com/dl/basic/Eg4.java
@@ -0,0 +1,14 @@
+package com.dl.basic;
+import java.util.*;
+
+public class Eg4 {
+
+	public static void main(String[] args) {
+    Scanner obj= new Scanner (System.in);
+    System.out.println("enter ");
+    int a=obj.nextInt();
+    System.out.println("you are enter "+a);
+
+	}
+
+}
